3794a2788c0101723580b259eb2e02abaa159b88
Zamcybermage <zamemdesigns@gmail.com>
2018-10-22 15:59:06 -0500

    readme added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-function $string(greet){
-  return greet;
-}
+/* Create a DIV */
+var div = document.createElement('DIV');
 
-alert $string("Hello World");
+/* Setting height of div */
+div.style.height = '100vh';
+
+/* Appending element to DOM */
+document.appendChild(div);
+/* console.log(div); */
+
+/* Adding event listener to do effects */
+
+div.addEventListener('mousemove', function(event){
+ console.log(event);
+ var x = event.clientX;
+ var y = event.clientY;
+ div.textContent = x + ', ' + y;
+ div.style.backgroundColor = 'rgb('+ x + ',' + y + ', 100)';
+
+  });
