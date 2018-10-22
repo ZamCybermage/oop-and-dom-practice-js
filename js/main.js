ac4ccba26fbdbbf9467761540f9931b1e210961b
Zamcybermage <zamemdesigns@gmail.com>
2018-10-22 16:28:06 -0500

    Changes made

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
+/* =====Part1---------------------------------------------------------- */
+
+/* MANIPULATING THE DOM */
+
 /* Create a DIV */
-var div = document.createElement('DIV');
+/* var div = document.createElement('DIV'); */
 
 /* Setting height of div */
-div.style.height = '100vh';
+/* div.style.height = '100vh'; */
 
 /* Appending element to DOM */
-document.appendChild(div);
+/* document.body.appendChild(div); */
 /* console.log(div); */
 
 /* Adding event listener to do effects */
 
-div.addEventListener('mousemove', function(event){
+/* div.addEventListener('mousemove', function(event){
  console.log(event);
- var x = event.clientX;
- var y = event.clientY;
+ var x = event.clientX; */
+/* var y = event.clientY;
  div.textContent = x + ', ' + y;
  div.style.backgroundColor = 'rgb('+ x + ',' + y + ', 100)';
 
-  });
+  }); */
+
+/* ===========Part2--------------------------------------------------------- */
+
+/* ==============USING FUNCTIONS====================== */
+
+/* -----Create DIV element------ */
+
+function fullScreen(element) {
+    var newDiv = document.createElement(element);
+    newDiv.style.height = "100vh";
+    document.body.appendChild(newDiv);
+    return newDiv;
+}
+
+function Input(inputType, DOMElement, callback) {
+  div.addEventListener("mousemove", function(event) {
+      var x = event.clientX;
+      var y = event.clientY;
+      div.textContent = x + ", " + y;
+      div.style.backgroundColor = "rgb(" + x + "," + y + ", 100)";
+    });
+}
